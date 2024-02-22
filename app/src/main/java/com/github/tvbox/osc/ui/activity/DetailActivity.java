74e777ee7d3892c4406043bb3175ea652856316b
sayxiaoyan <127412316+sayxiaoyan@users.noreply.github.com>
2024-02-23 02:11:27 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -188,7 +188,7 @@ public class DetailActivity extends BaseActivity {
         mGridViewFlag.setAdapter(seriesFlagAdapter);
         isReverse = false;
         firstReverse = false;
-        HawkConfig.MP3_BG= "";
+        Hawk.put(HawkConfig.MP3_BG, "");
         preFlag = "";
         if (showPreview) {
             playFragment = new PlayFragment();
@@ -667,7 +667,7 @@ public class DetailActivity extends BaseActivity {
 
                         if(vodInfo.seriesMap.get(vodInfo.playFlag).get(0).url.endsWith(".mp3")){//保存设置mp3背景
                             if(!Hawk.get(HawkConfig.MP3_BG, "").isEmpty()) {
-                                HawkConfig.MP3_BG= "";
+                                Hawk.put(HawkConfig.MP3_BG, "");
                             }			
                            Hawk.put(HawkConfig.MP3_BG, mVideo.pic);
                         }
